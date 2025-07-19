f294abd1338b0c389f25d5bda72a51641711e189
Trishul <tbose5855@gmail.com>
2025-07-19 15:23:57 +0530

    Added option to scan a particular subdomain to gather information

--- subdomain_finder.py
+++ subdomain_finder.py
@@ -1,56 +1,56 @@
 import dns.resolver
 from concurrent.futures import ThreadPoolExecutor
-
-def check_subdomain(subdomain, domain): #checks whether the subdomain exits or not
-    
-    full_domain = f"{subdomain}.{domain}"   #combines the prefix and sufix to form a working domain
-
-    try:
-        answers = dns.resolver.resolve(full_domain, 'A')    #call the resolve method to get the ip address using the A record
-        ips = [answer.to_text() for answer in answers]
-        print(f"[+] Found: {full_domain} -> {', '.join(ips)}")  #which returns the ip of the domain if it exists or else it gives an error
-        return((full_domain,ips))
-
-    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.resolver.LifetimeTimeout, dns.resolver.NoNameservers):
-        return None
-
-def find_subdomain(domain, wordlist, max_threads=100):
-
-    with open(wordlist, 'r') as file:
-        subdomains = [line.strip() for line in file if line.strip()]
-
-        results = []
-
-        try:
-
-            with ThreadPoolExecutor(max_workers=max_threads) as executor:
-                futures = [executor.submit(check_subdomain, subdomain, domain) for subdomain in subdomains]
-
-                for future in futures:
-                    result = future.result()
-
-                    if result:
-                        results.append(result)
-
-        except KeyboardInterrupt:
-            print("\nInterrupted by the user. Stopping...")
-            executor.shutdown(wait=False, cancel_futures=True)
-        
-    return results
+from domain_info_gather import subdomain_enumeration
 
         
 if __name__ == "__main__":
     
-    try:
-        print("Enter the target domain below")
-        print("eg: Exmaple.com")
-        domain = input(">> ")
+    print("🔍 This is a Subdomain Enumeration Tool")
 
-        wordlist = "subdomains-top1million-110000.txt"
+    enumerator = subdomain_enumeration()
 
-        found = [find_subdomain(domain, wordlist)]
+    while(True):
 
-        print(f"{len(found)} Subdomains found!!")
+        print("What do you want to do ?")
+        print("1. Check the available Subdomain for a particular domain")
+        print("2. Find out more info on a particular subdomain")
+        choice = int(input(">> "))
 
-    except KeyboardInterrupt:
-        print("\n[!] Script interrupted.")
+        if choice == 1:
+            try:
+                print("Enter the target domain below")
+                print("eg: Exmaple.com")
+                print("Or you can type 'exit' to exit out")
+                domain = input(">> ")
+
+                if domain.lower() == "exit":
+                    exit()
+
+                wordlist = "subdomains1.txt"
+
+                found = [enumerator.find_subdomain(domain, wordlist)]
+
+                print(f"{len(found)} Subdomains found!!")
+
+            except KeyboardInterrupt:
+                print("\n[!] Script interrupted.")
+                exit()
+
+
+        elif choice == 2:
+
+                print("Enter the subdomain below")
+                subdomain = input(">> ")
+
+                print(f"Subdomain Name: {subdomain}")
+
+                enumerator.check_HTTP(subdomain=subdomain)
+                enumerator.check_CNAME(subdomain=subdomain)
+                enumerator.check_MX(subdomain=subdomain)
+                enumerator.check_TXTrecord(subdomain=subdomain)
+            
+
+
+        else:
+            print("Wrong Input!!")
+            exit()
